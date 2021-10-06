09023864c4bb7b6dc93ce87844445332bd383212
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-06 11:08:45 +0700

    b2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte a = 10;
+        int b = 100;
+        short c = 10000;
+        long d = 1000000000;
+        float e = 2.5f;
+        double f = 2.4444444444444d;
+        boolean g = true;
+        char r = 'a';
+        String s = "hà nội";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(r);
+        System.out.println(s);
+
     }
 }
