b75685bc5e4145530a7b3eda9f78b338dbd5bc9a
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-06 10:52:45 +0700

    done

---
+++ src/com/company/SystemTime.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
